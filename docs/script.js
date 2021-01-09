68154d29c46b00f22e8552b5c4a61114073b8ee7
Wesley Beckner <wesleybeckner@gmail.com>
2021-01-09 17:38:59 -0600

    new animations

---
+++ docs/script.js
@@ -0,0 +1,28 @@
+// function myFunction() {
+//   setTimeout(function(){ 
+//     oonii = document.getElementById("oonii");
+//     oonii.innerHTML = "admin@oonii.io"
+//     }, 3000);
+// }
+// myFunction();
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.a');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.a .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  })
+
+// function newtext()
+// {
+
+// document.getElementById('oonii').innerHTML = "admin@oonii.io";
+// }
+
+// setTimeout(newtext,3000);
